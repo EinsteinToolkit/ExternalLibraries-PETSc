b5fff4b5c8e56982770821b7c08d8c18f7dd7aa5
Roland Haas <rhaas@mail.ubc.ca>
2025-07-16 16:23:15 -0700

    PETSc: update tarball to 3.23.4
    this allows one to build with Python 3.13

--- src/build.sh
+++ src/build.sh
@@ -14,7 +14,7 @@ set -e                          # Abort on errors
 
 # Set locations
 THORN=PETSc
-NAME=petsc-3.21.5
+NAME=petsc-3.23.4
 SRCDIR="$(dirname $0)"
 BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
 if [ -z "${PETSC_INSTALL_DIR}" ]; then
